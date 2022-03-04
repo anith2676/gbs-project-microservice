9eb3cfc999677ea46434488c4afdea98388f7726
anith <94350795+anith2676@users.noreply.github.com>
2022-03-04 18:09:55 +0530

    update

--- user-login-service/src/main/java/com/gbs/userloginservice/UserLoginServiceApplication.java
+++ user-login-service/src/main/java/com/gbs/userloginservice/UserLoginServiceApplication.java
@@ -2,8 +2,11 @@ package com.gbs.userloginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class UserLoginServiceApplication {
 
 	public static void main(String[] args) {
